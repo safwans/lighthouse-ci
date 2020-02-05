64926ea7646c1e4786e7540f8090b8b00e8238c3
Safwan Samla <safwans@safwans-macbookpro.roam.corp.google.com>
2020-02-05 12:17:29 -0500

    Updates

---
+++ precache-manifest.67a85386228d8bfa032e0fd4d864a524.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef3c2925843340daeac603be7465ab45",
+    "url": "/lighthouse-ci/index.html"
+  },
+  {
+    "revision": "54b1a5985fb9df44f609",
+    "url": "/lighthouse-ci/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0d8c316b8214f4cd18d7",
+    "url": "/lighthouse-ci/static/js/2.0101c52c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/lighthouse-ci/static/js/2.0101c52c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54b1a5985fb9df44f609",
+    "url": "/lighthouse-ci/static/js/main.f9b61ac2.chunk.js"
+  },
+  {
+    "revision": "828a9136be4cb4fd91a9",
+    "url": "/lighthouse-ci/static/js/runtime-main.b3452608.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lighthouse-ci/static/media/logo.5d5d9eef.svg"
+  }
+]);
